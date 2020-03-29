b51c48cc31f8173e3b569f3b09da3741e3881bbc
dojutsu-user <vaibhgupt199@gmail.com>
2020-03-29 14:59:03 +0530

    fix slug generation

--- src/announcements/utils.py
+++ src/announcements/utils.py
@@ -12,14 +12,14 @@ def unique_slug_generator(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     else:
-        slug = slugify(instance.title)
+        slug = slugify(instance.title)[:46]
 
     Klass = instance.__class__
-    qs_exists = Klass.objects.filter(slug=slug).exists()
+    qs_exists = Klass.objects.filter(slug=slug[:50]).exists()
     if qs_exists:
         new_slug = "{slug}-{randstr}".format(
-                    slug=slug,
+                    slug=slug[:46],
                     randstr=random_string_generator(size=4)
                 )
         return unique_slug_generator(instance, new_slug=new_slug)
-    return slug
+    return slug[:50]
