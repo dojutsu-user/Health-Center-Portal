42bc5813e32baa1a6b99710c975ca40d4c2b3ede
dojutsu-user <vaibhgupt199@gmail.com>
2020-09-14 20:35:52 +0530

    add comments and update .gitignore

--- dockerfiles/docker-entrypoint.test.sh
+++ dockerfiles/docker-entrypoint.test.sh
@@ -2,6 +2,9 @@
 pip install tox==3.20.0
 pip install codecov
 tox -e${PYTHON_VERSION}
+
+# For pushing the coverage report to codecov.io
+# (will not work in local).
 curl -s https://codecov.io/bash > codecov.sh
 chmod +x codecov.sh
 ./codecov.sh
